55558d38c025a13de3062a5bc4619eedf7e96274
ModderBoy <142475355+modderboyy@users.noreply.github.com>
2025-07-08 11:31:08 +0000

    Update constants.js

--- utils/constants.js
+++ utils/constants.js
@@ -1,10 +1,7 @@
+
 const BOT_TOKEN = "8057847116:AAEi6wpnJ9XMKegloX8DOzSzYC22ATKoDVs";
-const SUPABASE_URL =
-    process.env.NEXT_PUBLIC_SUPABASE_URL ||
-    "https://tdfphvmmwfqhnzfggpln.supabase.co";
-const SUPABASE_ANON_KEY =
-    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY ||
-    "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InRkZnBodm1td2ZxaG56ZmdncGxuIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTE3MzcwNjYsImV4cCI6MjA2NzMxMzA2Nn0.0H8_6f07k0vmjOVnqqXgqBYwIEu50Qqs_tExPv1k7DQ";
+const SUPABASE_URL = "https://tdfphvmmwfqhnzfggpln.supabase.co";
+const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InRkZnBodm1td2ZxaG56ZmdncGxuIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTE3MzcwNjYsImV4cCI6MjA2NzMxMzA2Nn0.0H8_6f07k0vmjOVnqqXgqBYwIEu50Qqs_tExPv1k7DQ";
 
 // Admin IDs - Replace with real admin Telegram IDs
 const ADMIN_IDS = [6295092422]; // Real admin Telegram IDs
